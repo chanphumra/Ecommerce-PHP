3cb4a4c0804d58ccce213d1eed9f5dca8caab708
chanphumra <chanphumracambodia@gmail.com>
2023-04-02 19:30:20 +0700

    completed check user

--- auth/checkuser.php
+++ auth/checkuser.php
@@ -6,7 +6,7 @@
         discount_price: 0,
         total: 0
     };
-    if(localStorage.getItem("telegram_id") || localStorage.getItem("token") || sessionStorage.getItem("email") && c.products.length > 0 ){
+    if((localStorage.getItem("telegram_id") || localStorage.getItem("token") || sessionStorage.getItem("email")) && c.products.length > 0 ){
        is_login = true;    
     }
     if(!is_login) window.location = 'index.php';
